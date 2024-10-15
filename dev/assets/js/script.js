01ccac2996580fea89e1401539bc6c6ff00d66cb
HyryaRoman <hyrya.roman@gmail.com>
2024-10-15 17:22:48 +0300

    script info-map

--- dev/assets/js/script.js
+++ dev/assets/js/script.js
@@ -1,1 +1,56 @@
+{
+  function setInfo(source, target) {
+    // Get all info slots
+    const slots = target.querySelectorAll('[data-info-slot]');
 
+    // For each info slot
+    slots.forEach((slot) => {
+      // Get attribute it displays
+      const dataSource = 'data-' + slot.getAttribute('data-info-slot');
+
+      // Get attributes value
+      const data = source.getAttribute(dataSource);
+
+      // Set inner html to retrieved value
+      slot.innerHTML = data;
+    })
+  }
+
+  function clearInfo(target) {
+    // Get all info slots
+    const slots = target.querySelectorAll('[data-info-slot]');
+
+    // Get clear text
+    const emptyText = target.getAttribute('data-info-empty') || '-';
+
+    // For each info slot
+    slots.forEach((slot) => {
+
+      // Set inner html to clear text
+      slot.innerHTML = emptyText;
+    })
+  }
+
+  function setupInfoMap(map) {
+    // Find info panel by given target attribute
+    const display = document.querySelector(map.getAttribute('data-target'));
+
+    // Get all options
+    const options = map.children;
+
+    // For each option
+    for (let i = 0; i < options.length; i++) {
+      const option = options[i];
+
+      // Set info when mouse enters
+      option.addEventListener("mouseover", (e) => setInfo(option, display));
+
+      // And clear it when it leaves
+      option.addEventListener("mouseout", (e) => clearInfo(display));
+    }
+
+    clearInfo(display);
+  }
+
+  document.querySelectorAll('.info-map').forEach(setupInfoMap);
+}
